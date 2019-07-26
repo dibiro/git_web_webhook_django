85f1312dba724f3fc4e61b8796adedd9254abb7b
leopoldo ramon motesinos moreno <leopoldodibiro@gmail.com>
2019-07-26 11:13:38 -0500

    update

--- git_web_webhook_django/command/admin.py
+++ git_web_webhook_django/command/admin.py
@@ -10,7 +10,25 @@ class ProjectAdmin(admin.ModelAdmin):
     list_filter = ('create_date', 'is_active')
     list_display = ['name', 'git_name', 'is_active',] 
 
+
+class UpdateItemAdmin(admin.ModelAdmin):
+    search_fields = (
+        'update__project__name',
+    )
+    list_filter = ('create_date', 'update')
+    list_display = ['update', 'create_date'] 
+
+
+class CommandAdmin(admin.ModelAdmin):
+    search_fields = (
+        'name',
+        'project__name',
+    )
+    list_filter = ('project')
+    list_display = ['project', 'name', 'create_date'] 
+
+
 admin.site.register(Project, ProjectAdmin)
-admin.site.register(Command)
+admin.site.register(Command, CommandAdmin)
 admin.site.register(Update)
-admin.site.register(UpdateLog)
+admin.site.register(UpdateLog, UpdateItemAdmin)
